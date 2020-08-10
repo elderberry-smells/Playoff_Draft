2339733f342e019d0c9542d51a87904e57ca1981
Brian James <brianjames567@gmail.com>
2020-08-10 15:42:07 -0600

    Update __init__.py

--- nhl/nhl/__init__.py
+++ nhl/nhl/__init__.py
@@ -6,13 +6,13 @@ from flask_mail import Mail
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '793d3b79e81f45b0faeec49bd844b477'
+app.config['SECRET_KEY'] = 'secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'thisis100percentnotfake@gmail.com'
-app.config['MAIL_PASSWORD'] = 'etxp afzg fpqf exup'
+app.config['MAIL_USERNAME'] = 'your apps email@gmail.com'
+app.config['MAIL_PASSWORD'] = 'emails password'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
